1652ba5246538ae26b329e4226b8f837c796aa9d
bcantor2000 <bcantor@usf.edu>
2022-02-22 14:48:30 -0500

    Add project files.

---
+++ T-2 Cantor correct framework/Program.cs
@@ -0,0 +1,20 @@
+﻿//Brian Cantor
+//1/24/2022
+//Tech Assignmnet #2 - Basic Console App
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_2_Cantor_correct_framework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("One day I am looking down at the screen, next thing you know the screen is looking down at you" + "\n" + "-Brian Cantor");
+            Console.ReadLine();
+        }
+    }
+}
